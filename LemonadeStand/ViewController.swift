25c9239e17442f09d1df1f65b70c776345b95cf1
Brett Wallace <bwallace90@gmail.com>
2015-03-09 22:35:24 -0400

    Call updateMainView when app is loaded

--- LemonadeStand/ViewController.swift
+++ LemonadeStand/ViewController.swift
@@ -29,6 +29,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateMainView()
     }
 
     override func didReceiveMemoryWarning() {
