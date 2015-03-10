6594857cdf7e746639cd8b8a29288c97ec474e12
Brett Wallace <bwallace90@gmail.com>
2015-03-09 22:14:40 -0400

    Add Price struct with constants

---
+++ LemonadeStand/Price.swift
@@ -0,0 +1,14 @@
+//
+//  Price.swift
+//  LemonadeStand
+//
+//  Created by Brett Wallace on 3/9/15.
+//  Copyright (c) 2015 Brett Wallace. All rights reserved.
+//
+
+import Foundation
+
+struct Price {
+    let lemon = 2
+    let iceCube = 1
+}
