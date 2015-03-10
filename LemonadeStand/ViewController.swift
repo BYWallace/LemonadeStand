8647d657355e9cd738492b15cc1836d576e12e36
Brett Wallace <bwallace90@gmail.com>
2015-03-09 21:59:19 -0400

    Add connections to view controller from storyboard

--- LemonadeStand/ViewController.swift
+++ LemonadeStand/ViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var moneySupplyCount: UILabel!
+    @IBOutlet weak var lemonSupplyCount: UILabel!
+    @IBOutlet weak var iceCubeSupplyCount: UILabel!
+    @IBOutlet weak var lemonPurchaseCount: UILabel!
+    @IBOutlet weak var iceCubePurchaseCount: UILabel!
+    @IBOutlet weak var lemonMixCount: UILabel!
+    @IBOutlet weak var iceCubeMixCount: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +26,34 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // IBActions
+    @IBAction func purchaseLemonButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func purchaseIceCubeButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func unpurchaseLemonButtonPressed(sender: UIButton) {
+    }
 
-
+    @IBAction func unpurchaseIceCubeButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func mixLemonButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func mixIceCubeButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func unmixLemonButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func unmixIceCubePressed(sender: UIButton) {
+    }
+    
+    @IBAction func startDayButtonPressed(sender: UIButton) {
+    }
+    
 }
 
